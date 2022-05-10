588a89002bf976940cb714efd26893e3cccd87b3
wbrinksma <29278380+wbrinksma@users.noreply.github.com>
2022-05-11 01:14:37 +0200

    - Added download text function

---
+++ src/Helpers/DownloadHelper.ts
@@ -0,0 +1,7 @@
+export default function downloadTextFile(text, name) {
+    const a = document.createElement('a');
+    const type = name.split(".").pop();
+    a.href = URL.createObjectURL(new Blob([text], { type: `text/${type === "txt" ? "plain" : type}` }));
+    a.download = name;
+    a.click();
+}
